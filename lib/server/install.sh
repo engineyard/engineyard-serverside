8be832bcc55987aea16aad22f664a037e3b01a23
Ezra Zygmuntowicz <ez@engineyard.com>
2010-01-12 22:34:41 -0800

    make ey-deploy-install idempotent, so that you can run it over and over again and it will always just install the new version

--- lib/server/install.sh
+++ lib/server/install.sh
@@ -11,18 +11,21 @@ our_script_path="$(dirname $0)/git-receive-pack"
 post_receive_hook_path="$(dirname $0)/post-receive"
 post_receive_hook_new_path="$real_script_dir/engineyard_post-receive_hook"
 
+if [[ ! -w "$real_script_path" ]]; then
+	echo "You have to run this as root."
+	exit 1
+else 
+
 
 if [[ -f "$real_script_new_path" ]]; then
-	echo "$real_script_new_path already exists"
+	cp $post_receive_hook_path $post_receive_hook_new_path
+	chmod +x $post_receive_hook_new_path
+	echo 'engineyard git hooks installed'
 else
-	if [[ ! -w "$real_script_path" ]]; then
-		echo "You don't have permission to modify $real_script_path"
-	else 
-		mkdir -p $real_script_new_dir
-		mv $real_script_path $real_script_new_path
-		cp $our_script_path $real_script_path
-		cp $post_receive_hook_path $post_receive_hook_new_path
-		chmod +x $post_receive_hook_new_path
-		echo 'engineyard git hooks installed'
-	fi
+	mkdir -p $real_script_new_dir
+	mv $real_script_path $real_script_new_path
+	cp $our_script_path $real_script_path
+	cp $post_receive_hook_path $post_receive_hook_new_path
+	chmod +x $post_receive_hook_new_path
+	echo 'engineyard git hooks installed'
 fi
