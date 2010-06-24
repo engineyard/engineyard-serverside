0764f2a5b249302f5bdf4d620030591437173ffc
Sam Merritt <smerritt@engineyard.com>
2010-06-24 15:59:13 -0700

    Take instance-id out of eysd output.
    Since all the commands run from the app master, it's just noise.
    
    [#75]

--- lib/ey-deploy/verbose_system.rb
+++ lib/ey-deploy/verbose_system.rb
@@ -1,11 +1,7 @@
 module EY
   module VerboseSystem
-    def self.instance_id
-      @instance_id ||= `curl http://169.254.169.254/latest/meta-data/instance-id 2>/dev/null`
-    end
-
     def system(cmd)
-      puts "::   #{EY::VerboseSystem.instance_id} running #{cmd}"
+      puts "::   running #{cmd}"
       super
     end
   end
