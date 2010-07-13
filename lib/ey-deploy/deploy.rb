engineyard/engineyard-serverside

commit c2c25c758aabe3e6f2ddacd47f8e3d6b54829b04

s/eysd/ey-deploy/
[#141 state:merged responsible:none]

lib/ey-deploy/deploy.rb
@@ -213,9 +213,9 @@ module EY
 
     def callback(what)
       @callbacks_reached ||= true
-      if File.exist?("#{c.release_path}/deploy/#{what}.rb")
-        eysd_path = $0   # invoke others just like we were invoked
-        run "#{eysd_path} _#{VERSION}_ hook '#{what}' --app '#{config.app}' --release-path #{config.release_path}" do |server, cmd|
+      if File.exist?("#{c.release_release}/deploy/#{what}.rb")
+        eydeploy_path = $0   # invoke others just like we were invoked
+        run "#{eydeploy_path} _#{VERSION}_ hook '#{what}' --app '#{config.app}' --release-path #{config.release_path}" do |server, cmd|
           cmd << " --framework-env '#{c.environment}'"
           cmd << " --current-role '#{server.role}'"
           cmd << " --current-name '#{server.name}'" if server.name

ey-deploy.gemspec
@@ -13,8 +13,8 @@ Gem::Specification.new do |s|
   s.summary = "A gem that deploys ruby applications on EY Cloud instances"
 
   s.files = Dir.glob("{bin,lib}/**/*") + %w(LICENSE)
-  s.executables = ["eysd"]
-  s.default_executable = "eysd"
+  s.executables = ["ey-deploy"]
+  s.default_executable = "ey-deploy"
   s.require_path = 'lib'
 
   s.rubygems_version = %q{1.3.6}

lib/ey-deploy/version.rb
@@ -1,3 +1,3 @@
 module EY
-  VERSION = '0.8.3.pre'
+  VERSION = '0.9.0.pre'
 end

commit 790ab3e6021f9156c5853ac11be3a57c3c3772d6

fix typo introduced in c2c25c75

lib/ey-deploy/deploy.rb
@@ -213,7 +213,7 @@ module EY
 
     def callback(what)
       @callbacks_reached ||= true
-      if File.exist?("#{c.release_release}/deploy/#{what}.rb")
+      if File.exist?("#{c.release_path}/deploy/#{what}.rb")
         eydeploy_path = $0   # invoke others just like we were invoked
         run "#{eydeploy_path} _#{VERSION}_ hook '#{what}' --app '#{config.app}' --release-path #{config.release_path}" do |server, cmd|
           cmd << " --framework-env '#{c.environment}'"