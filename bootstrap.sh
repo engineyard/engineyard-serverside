f60152c900661969ecc9d9acbc8c1d8019c62c6f
Joseph Hsu <jhsu.x1@gmail.com>
2013-08-02 13:08:26 -0400

    add stub deploy script in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,7 @@ apt-get update
 apt-get install -y ruby1.9.3 unzip curl
 
 mkdir -p /data/serverside && chown vagrant:vagrant /data/serverside
+mkdir -p /engineyard/bin
+
+echo "echo hello" > /engineyard/bin/app_serverside
+chmod +x /engineyard/bin/app_serverside
