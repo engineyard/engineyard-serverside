dda0fd1ac91e39504e1270f52bb0aaa234336849
rbankston <rbankston@engineyard.com>
2017-03-29 23:34:51 -0400

    Move the symlink order

--- lib/engineyard-serverside/dependency_manager/mix.rb
+++ lib/engineyard-serverside/dependency_manager/mix.rb
@@ -19,9 +19,9 @@ module EY
           def install
             shell.status "Installing  packages (mix.ex detected)"
             run "mkdir -p #{paths.elixir_deps} && ln -nfs #{paths.elixir_deps} #{paths.active_elixir_deps}"
+            run %{ln -nfs #{paths.shared_config}/prod.secret.exs #{paths.active_release_config}/prod.secret.exs}
+            run %{ln -nfs #{paths.shared_config}/customer.secret.exs #{paths.active_release_config}/customer.secret.exs}
             run %{cd #{paths.active_release} && export GIT_SSH="#{ENV['GIT_SSH']}" && mix deps.get #{mix_install_options.join(" ")}}
-            run %{cd ln -nfs #{paths.shared_config}/prod.secret.exs #{paths.active_release_config}/prod.secret.exs}
-            run %{cd ln -nfs #{paths.shared_config}/customer.secret.exs #{paths.active_release_config}/customer.secret.exs}
             run %{cd #{paths.active_release} && #{elixir_compile} && rsync -avHl #{paths.active_release}/rel #{paths.elixir_rel}}
           end
 
@@ -35,8 +35,7 @@ module EY
           def mix_production?
             ENV['MIX_ENV'] == 'prod'
           end
+
         end
       end
     end
-  end
-end
