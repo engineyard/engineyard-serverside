22f8db6b1659580005ad2b7b33e10621a99c7207
rbankston <rbankston@engineyard.com>
2017-04-20 11:10:21 -0400

    Working on tests

--- spec-outside/elixir_deploy_spec.rb
+++ spec-outside/elixir_deploy_spec.rb
@@ -1,1 +1,25 @@
 require 'spec_helper'
+
+describe "Deploying an application that uses Mix" do
+pending
+end
+
+describe "Deploying an application that uses distillery and phoenix" do
+  pending
+end
+
+describe "Deploying an application that uses exrm and phoenix" do
+  pending
+end
+
+describe "Deploying an application that uses distillery but not phoenix" do
+  pending
+end
+
+describe "Deploying an application that uses exrm but not phoenix" do
+  pending
+end
+
+describe "Failing to deploy an application that doesn't use exrm or distillery" do
+  pending ("Failure code to kill the deploy when the check is run")
+end
