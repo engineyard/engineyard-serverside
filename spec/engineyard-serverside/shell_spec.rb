f82f0b6fb0caecfaf20e647f9d88b151fffc5372
Dennis Walters <pooster@gmail.com>
2020-02-05 02:39:26 -0500

    Adds unit tests for EY::Serverside::Shell
    Additionally, dorked the whitespace in the class file itself for
    the sake of readability.

--- spec/engineyard-serverside/shell_spec.rb
+++ spec/engineyard-serverside/shell_spec.rb
@@ -41,6 +41,18 @@ module EY
         allow(Logger).to receive(:new).and_return(logger)
         allow(logger).to receive(:formatter=)
         allow(logger).to receive(:level=)
+        allow(logger).to receive(:info)
+        allow(logger).to receive(:fatal)
+        allow(logger).to receive(:error)
+        allow(logger).to receive(:debug)
+        allow(logger).to receive(:unknown)
+
+        # So, Object has a private #warn method, so we have to get crafty to stub it.
+        # Also, this is dirty as all heck.
+        logger.instance_eval do
+          def warn(*args)
+          end
+        end
 
         # Let's go ahead and make sure we can predict the time, too.
         Timecop.freeze(now_is_the_time)
@@ -255,37 +267,272 @@ module EY
             expect(result).to be_falsey
           end
         end
-
-
       end
 
-      describe '#status'
+      describe '#status' do
+        let(:msg) {Object.new}
+        let(:gsubbed) {Object.new}
+        let(:result) {shell.status(msg)}
 
-      describe '#substatus'
+        before(:each) do
+          allow(msg).to receive(:gsub).and_return(gsubbed)
+        end
 
-      describe '#fatal'
+        context 'when the messgae supports forced encoding' do
+          before(:each) do
+            allow(msg).to receive(:force_encoding)
+          end
 
-      describe '#error'
+          it 'forces the encoding to UTF-8' do
+            expect(msg).to receive(:force_encoding).with(Encoding::UTF_8)
 
-      describe '#warning'
+            result
+          end
+        end
 
-      describe '#warn'
+        it 'puts the status prefix at the beginning of each line in the message' do
+          expect(msg).
+            to receive(:gsub).
+            with(described_class::BOL, described_class::STATUS_PREFIX).
+            and_return(gsubbed)
 
-      describe '#notice'
+          result
+        end
 
-      describe '#info'
+        it 'logs the modified message as an info line' do
+          expect(logger).to receive(:info).with(gsubbed)
 
-      describe '#debug'
+          result
+        end
+      end
 
-      describe '#unknown'
+      describe '#substatus' do
+        let(:msg) {Object.new}
+        let(:gsubbed) {Object.new}
+        let(:result) {shell.substatus(msg)}
 
-      describe '#command_show'
+        before(:each) do
+          allow(msg).to receive(:gsub).and_return(gsubbed)
+        end
 
-      describe '#comand_out'
+        it 'adds the substatus prefix to the beginning of each line in the message' do
+          expect(msg).
+            to receive(:gsub).
+            with(described_class::BOL, described_class::SUBSTATUS_PREFIX).
+            and_return(gsubbed)
 
-      describe '#command_err'
+          result
+        end
 
-      describe '#logged_system'
+        it 'logs the modified message as a debug line' do
+          expect(logger).to receive(:debug).with(gsubbed)
+
+          result
+        end
+      end
+
+      describe '#fatal' do
+        let(:msg) {'finish him!'}
+        let(:result) {shell.fatal(msg)}
+
+        it 'forwards the call to the logger after decorating the message' do
+          expect(logger).to receive(:fatal).with("FATAL: #{msg}")
+
+          result
+        end
+      end
+
+      describe '#error' do
+        let(:msg) {'you do not know the way'}
+        let(:result) {shell.error(msg)}
+
+        it 'forwards the call to the logger after decorating the message' do
+          expect(logger).to receive(:error).with("ERROR: #{msg}")
+
+          result
+        end
+      end
+
+      describe '#warning' do
+        let(:msg) {'she knew that her life had passed her by'}
+        let(:result) {shell.warning(msg)}
+
+        it 'forwards the call to the logger after decorating the message' do
+          expect(logger).to receive(:warn).with("WARNING: #{msg}")
+
+          result
+        end
+      end
+
+      describe '#warn' do
+        let(:msg) {'watch out for that tree'}
+        let(:result) {shell.warn(msg)}
+
+        it 'forwards the call to the logger after decorating the message' do
+          expect(logger).to receive(:warn).with("WARNING: #{msg}")
+
+          result
+        end
+      end
+
+      describe '#notice' do
+        let(:msg) {'of intent'}
+        let(:result) {shell.notice(msg)}
+
+        it 'warns unmodified via the logger' do
+          expect(logger).to receive(:warn).with(msg)
+
+          result
+        end
+      end
+
+      describe '#info' do
+        let(:msg) {'rmation'}
+        let(:result) {shell.info(msg)}
+
+        it 'forwards the call to the logger unmodified' do
+          expect(logger).to receive(:info).with(msg)
+
+          result
+        end
+      end
+
+      describe '#debug' do
+        let(:msg) {'a buggy bug on a bug made of bugs'}
+        let(:result) {shell.debug(msg)}
+
+        it 'forwards the call to the logger unmodified' do
+          expect(logger).to receive(:debug).with(msg)
+
+          result
+        end
+      end
+
+      describe '#unknown' do
+        let(:msg) {'soldier'}
+        let(:result) {shell.unknown(msg)}
+
+        it 'forwards the call to the logger unmodified' do
+          expect(logger).to receive(:unknown).with(msg)
+
+          result
+        end
+      end
+
+      describe '#command_show' do
+        let(:cmd) {Object.new}
+        let(:continued) {Object.new}
+        let(:prefixed) {Object.new}
+        let(:result) {shell.command_show(cmd)}
+
+        before(:each) do
+          allow(cmd).to receive(:gsub).and_return(continued)
+          allow(continued).to receive(:sub).and_return(prefixed)
+        end
+
+        it 'adds the command continue decoration to each line of the message' do
+          expect(cmd).
+            to receive(:gsub).
+            with(described_class::BOL, described_class::CMD_CONTINUE).
+            and_return(continued)
+
+          result
+        end
+
+        it 'replaces the first command continue decoration with a command prefix' do
+          expect(continued).
+            to receive(:sub).
+            with(described_class::CMD_CONTINUE, described_class::CMD_PREFIX).
+            and_return(prefixed)
+
+          result
+        end
+
+        it 'logs the decorated message as a debug line via the logger' do
+          expect(logger).to receive(:debug).with(prefixed)
+
+          result
+        end
+      end
+
+      describe '#comand_out' do
+        let(:msg) {Object.new}
+        let(:indented) {Object.new}
+        let(:result) {shell.command_out(msg)}
+
+        before(:each) do
+          allow(msg).to receive(:gsub).and_return(indented)
+        end
+
+        it 'indents each line of the message' do
+          expect(msg).
+            to receive(:gsub).
+            with(described_class::BOL, described_class::CMD_INDENT).
+            and_return(indented)
+
+          result
+        end
+
+        it 'logs the indented message as a debug line via the logger' do
+          expect(logger).to receive(:debug).with(indented)
+
+          result
+        end
+      end
+
+      describe '#command_err' do
+        let(:msg) {Object.new}
+        let(:indented) {Object.new}
+        let(:result) {shell.command_err(msg)}
+
+        before(:each) do
+          allow(msg).to receive(:gsub).and_return(indented)
+        end
+
+        it 'indents each line of the message' do
+          expect(msg).
+            to receive(:gsub).
+            with(described_class::BOL, described_class::CMD_INDENT).
+            and_return(indented)
+
+          result
+        end
+
+        it 'logs the indented message as an unknown line via the logger' do
+          expect(logger).to receive(:unknown).with(indented)
+
+          result
+        end
+      end
+
+      describe '#logged_system' do
+        let(:cmd) {Object.new}
+        let(:spawn_result) {Object.new}
+        let(:result) {shell.logged_system(cmd)}
+
+        it 'spawns the command, passing the shell to the spawner' do
+          expect(EY::Serverside::Spawner).
+            to receive(:run).
+            with(cmd, shell, nil).
+            and_return(spawn_result)
+
+          expect(result).to eql(spawn_result)
+        end
+
+        context 'when a server is passed in' do
+          let(:server) {Object.new}
+          let(:result) {shell.logged_system(cmd, server)}
+
+          it 'passes the server along as well' do
+            expect(EY::Serverside::Spawner).
+              to receive(:run).
+              with(cmd, shell, server).
+              and_return(spawn_result)
+
+            expect(result).to eql(spawn_result)
+          end
+        end
+      end
     end
 
   end
